a790cc067e3d5b846d6fd2a2151fefa4a2b90a41
miciowl <miciowl@github.com>
2018-06-09 16:57:45 +0800

    導覽LOGO連結

---
+++ js/smooth-scroll.js
@@ -0,0 +1,16 @@
+$(function() {
+    $('a[href*="#"]:not([href="#"], [href="#dessert"],[href="#cake"],[href="#pie"],[href="#cookie"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            console.log(target);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1500);
+                return false;
+            }
+        }
+    });
+});
